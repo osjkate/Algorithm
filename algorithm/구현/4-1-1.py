883a1950bb028fddf6ac06899c1537b960c89fb2
sujinoh <osjkate@sookmyung.ac.kr>
2023-01-30 17:24:54 +0900

    상하좌우

---
+++ algorithm/구현/4-1-1.py
@@ -0,0 +1,23 @@
+n = int(input())
+plans = input().split()
+x, y = 1, 1
+nx, ny = 1, 1
+
+# L, R, U, D
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+move_types = ["L", "R", "U", "D"]
+
+for plan in plans:
+    for i in range(len(move_types)):
+        nx = x
+        ny = y
+        if plan == move_types[i]:
+            nx += dx[i]
+            ny += + dy[i]
+            if nx < 1 or ny < 1 or nx > n or ny > n:
+                continue
+            else:
+                x, y = nx, ny
+
+print(x, y)
