92aefb01bed327d9101a10e282b08b788114c6c4
osjkate <98140863+osjkate@users.noreply.github.com>
2023-02-17 17:28:56 +0900

    lambda 함수

---
+++ algorithmStudy/파이썬 기초 문법(선수지식)/12.람다함수.py
@@ -0,0 +1,25 @@
+'''
+람다 함수 == 익명 함수 == 람다 표현식
+
+
+
+def plus_one(x):
+    return x + 1
+
+
+print(plus_one(1))
+
+
+
+plus_two = lambda x: x + 2
+print(plus_two(1))
+'''
+
+
+def plus_one(x):
+    return x + 1
+
+
+a = [1, 2, 3]
+print(list(map(plus_one, a)))
+print(list(map(lambda x: x + 1, a)))
