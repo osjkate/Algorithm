6be4502be8063285e444becf20b662660c839be5
osjkate <98140863+osjkate@users.noreply.github.com>
2023-03-16 19:29:44 +0900

    스터디 - 5. 수들의 합

--- algorithmStudy/섹션3 탐색&시뮬레이션/5. 수들의 합.py
+++ algorithmStudy/섹션3 탐색&시뮬레이션/5. 수들의 합.py
@@ -1,16 +1,59 @@
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
-result = 0
+# result = 0
+# cnt = 0
+# for i in n:
+#     for j in (i, n):
+#         i += j
+#         if i == m:
+#             cnt += 1
+#             break
+#         elif i > m:
+#             break
+#
+#
+# print(cnt)
+
+# result = 0
+# cnt = 0
+# for i in range(n):
+#     result = a[i]
+#     for j in range(i + 1, n):
+#         print(result, cnt)
+#         if result == m:
+#             cnt += 1
+#             break
+#         elif result > m:
+#             break
+#         result += a[j]
+#
+# print(cnt)
+
+
+# 포인터 변수를 사용하기
+# tot이 목표보다 크고 작음을 비교해서 포인터를 이동시킴
+# 포인터가 배열의 수보다 커지면 while문을 빠져나오기
+lt = 0
+rt = 1
+tot = a[0]
 cnt = 0
-for i in n:
-    for j in (i, n):
-        i += j
-        if i == m:
-            cnt += 1
+
+
+while True:
+    if tot < m:
+        if rt < n:
+            tot += a[rt]
+            rt += 1
+        else:
             break
-        elif i > m:
-            break
+    elif tot == m:
+        cnt += 1
+        tot -= a[lt]
+        lt += 1
+    else:
+        tot -= a[lt]
+        lt += 1
 
 
 print(cnt)
